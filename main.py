dcc5ca7235988dfac6afa1050ee4c3c593a5a4f1
marower <32189304+marower@users.noreply.github.com>
2020-08-23 15:18:34 -0700

    Revert "Update main.py"
    This reverts commit 578a4fffd563d653190f3bb6a1ccc3051ddb2275.

--- main.py
+++ main.py
@@ -121,7 +121,7 @@ for card in oracle:
             cmc,
             art_uri])
 
-conn = mysql.connector.connect(host='localhost', user ='root', password = 'pass', db='mtg', port=3306)
+conn = mysql.connector.connect(host='localhost', user ='root', password = 'pass', db='mtg', port=3307)
 
 create_temp_table(conn)
 
